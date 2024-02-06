065ab2e6b79fd1fd282a379693a5f5613b6d016b
tristan99 <ariescacy9@gmail.com>
2024-02-05 22:59:01 -0500

    first commit

---
+++ reto_python_1.py
@@ -0,0 +1,7 @@
+nombres = input('ingrese su nombre: ')
+apellidos= input('ingrese sus apellidos: ')
+nombre_completo = nombres+' '+apellidos
+numero_telefono = input('ingrese su numero telefonico: ')
+correo_electronico = input('ingrese su correo: ')
+print('Hola '+nombre_completo+' ,en breve recibirás un correo a: '+correo_electronico)
+
